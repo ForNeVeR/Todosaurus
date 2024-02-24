53a858aec75b5cb7a0ea6fd98723e059dc28ebd7
Friedrich von Never <friedrich@fornever.me>
2024-02-24 20:48:55 +0100

    Project: bootstrap the main plugin properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -75,7 +75,6 @@ tasks {
 
     patchPluginXml {
         version = properties("pluginVersion")
-        sinceBuild = properties("pluginSinceBuild")
         untilBuild = properties("pluginUntilBuild")
 
         // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
