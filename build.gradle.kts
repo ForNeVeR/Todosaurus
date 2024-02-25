f42ce09613b50123150301fe48f1a3f993fd5d66
Friedrich von Never <friedrich@fornever.me>
2024-02-25 20:45:12 +0100

    Build: remove the UI testing mode

--- build.gradle.kts
+++ build.gradle.kts
@@ -105,15 +105,6 @@ tasks {
         }
     }
 
-    // Configure UI tests plugin
-    // Read more: https://github.com/JetBrains/intellij-ui-test-robot
-    runIdeForUiTests {
-        systemProperty("robot-server.port", "8082")
-        systemProperty("ide.mac.message.dialogs.as.sheets", "false")
-        systemProperty("jb.privacy.policy.text", "<!--999.999-->")
-        systemProperty("jb.consents.confirmation.enabled", "false")
-    }
-
     publishPlugin {
         dependsOn("patchChangelog")
         token = environment("PUBLISH_TOKEN")
