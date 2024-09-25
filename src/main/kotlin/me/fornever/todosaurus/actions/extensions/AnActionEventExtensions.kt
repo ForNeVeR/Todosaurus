05a7f025f18e10aa72441322fbbeb7a0f4a24040
Никита Великий <mtyoungstz@outlook.com>
2024-09-26 03:08:13 +0500

    Move extension methods for AnActionEvent to the separate file

---
+++ src/main/kotlin/me/fornever/todosaurus/actions/extensions/AnActionEventExtensions.kt
@@ -0,0 +1,30 @@
+package me.fornever.todosaurus.actions.extensions
+
+import com.intellij.ide.todo.TodoPanel
+import com.intellij.ide.todo.nodes.TodoItemNode
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.PlatformDataKeys
+import com.intellij.openapi.editor.RangeMarker
+import com.intellij.ui.treeStructure.Tree
+import com.intellij.util.ui.tree.TreeUtil
+
+fun AnActionEvent.getToDoTextRange(): RangeMarker? {
+    val tree = getData(PlatformDataKeys.CONTEXT_COMPONENT) as? Tree
+    val descriptor = tree?.selectionPath?.let(TreeUtil::getLastUserObject) as? TodoItemNode
+    return descriptor?.value?.rangeMarker
+}
+
+fun AnActionEvent.tryActivateThisAction(): Boolean {
+    val toDoPanel = getData(TodoPanel.TODO_PANEL_DATA_KEY)
+    val project = project
+
+    if (toDoPanel == null || project == null) {
+        presentation.isEnabledAndVisible = false
+
+        return false
+    }
+
+    presentation.isVisible = true
+
+    return true
+}
