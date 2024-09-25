195a8347858a541e57cc08b64df0540db81fe9c4
Никита Великий <mtyoungstz@outlook.com>
2024-09-26 03:08:13 +0500

    Move extension methods for AnActionEvent to the separate file

--- src/main/kotlin/me/fornever/todosaurus/actions/CreateIssueAction.kt
+++ src/main/kotlin/me/fornever/todosaurus/actions/CreateIssueAction.kt
@@ -1,14 +1,10 @@
 package me.fornever.todosaurus.actions
 
-import com.intellij.ide.todo.TodoPanel
-import com.intellij.ide.todo.nodes.TodoItemNode
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.actionSystem.PlatformDataKeys
-import com.intellij.openapi.editor.RangeMarker
-import com.intellij.ui.treeStructure.Tree
-import com.intellij.util.ui.tree.TreeUtil
+import me.fornever.todosaurus.actions.extensions.getToDoTextRange
+import me.fornever.todosaurus.actions.extensions.tryActivateThisAction
 import me.fornever.todosaurus.services.ToDoItem
 import me.fornever.todosaurus.services.ToDoService
 
@@ -17,14 +13,8 @@ class CreateIssueAction : AnAction() {
     override fun getActionUpdateThread() = ActionUpdateThread.BGT
 
     override fun update(e: AnActionEvent) {
-        val toDoPanel = e.getData(TodoPanel.TODO_PANEL_DATA_KEY)
-        val project = e.project
-        if (toDoPanel == null || project == null) {
-            e.presentation.isEnabledAndVisible = false
+        if (!e.tryActivateThisAction())
             return
-        }
-
-        e.presentation.isVisible = true
 
         val range = e.getToDoTextRange()
         e.presentation.isEnabled = range != null && ToDoItem(range).isNew
@@ -35,10 +25,4 @@ class CreateIssueAction : AnAction() {
         val range = e.getToDoTextRange() ?: return
         ToDoService.getInstance(project).showCreateIssueDialog(range)
     }
-}
-
-private fun AnActionEvent.getToDoTextRange(): RangeMarker? {
-    val tree = getData(PlatformDataKeys.CONTEXT_COMPONENT) as? Tree
-    val descriptor = tree?.selectionPath?.let(TreeUtil::getLastUserObject) as? TodoItemNode
-    return descriptor?.value?.rangeMarker
 }
