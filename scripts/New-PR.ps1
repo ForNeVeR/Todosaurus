ad8847bde142cbd590ce56370e60b4ecae6e5fcb
Friedrich von Never <friedrich@fornever.me>
2024-09-28 22:04:59 +0200

    CI: fixes and more logging in the PR action

--- scripts/New-PR.ps1
+++ scripts/New-PR.ps1
@@ -10,11 +10,13 @@ param (
 $ErrorActionPreference = 'Stop'
 Set-StrictMode -Version Latest
 
+Write-Output 'Configuring Git…'
 git config user.name $GitUserName
 if (!$?) { throw "Error running git config: $LASTEXITCODE." }
 git config user.email $GitUserEmail
 if (!$?) { throw "Error running git config: $LASTEXITCODE." }
 
+Write-Output 'Generating and pushing a branch…'
 git switch --force-create $BranchName
 if (!$?) { throw "Error running git switch: $LASTEXITCODE." }
 git commit --all --message $CommitMessage
@@ -22,6 +24,7 @@ if (!$?) { throw "Error running git commit: $LASTEXITCODE." }
 git push --set-upstream origin $BranchName
 if (!$?) { throw "Error running git push: $LASTEXITCODE." }
 
+Write-Output 'Creating a pull request…'
 gh pr create `
     --title $PrTitle `
     --body-file $PrBodyPath `
