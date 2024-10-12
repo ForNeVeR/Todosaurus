e395446294ad331f82467a1348f8e6cfe8e266aa
Никита Великий <mtyoungstz@outlook.com>
2024-10-12 22:27:47 +0500

    Refactor Notifications

---
+++ src/main/kotlin/me/fornever/todosaurus/ui/Notifications.kt
@@ -0,0 +1,37 @@
+package me.fornever.todosaurus.ui
+
+import com.intellij.notification.NotificationGroupManager
+import com.intellij.notification.NotificationType
+import com.intellij.openapi.project.Project
+import me.fornever.todosaurus.issues.IssueModel
+import me.fornever.todosaurus.ui.actions.OpenNewIssueInBrowserAction
+
+object Notifications {
+    object CreateNewIssue {
+        fun success(issue: IssueModel, project: Project) {
+            NotificationGroupManager
+                .getInstance()
+                .getNotificationGroup("TodosaurusNotifications")
+                .createNotification("Issue ${issue.number} has been created.", NotificationType.INFORMATION)
+                .addAction(OpenNewIssueInBrowserAction(issue))
+                .notify(project)
+        }
+
+        fun failed(project: Project) {
+            NotificationGroupManager
+                .getInstance()
+                .getNotificationGroup("TodosaurusNotifications")
+                .createNotification("Create new issue", "Failed to create issue", NotificationType.ERROR)
+                .notify(project)
+        }
+    }
+
+    object OpenReportedIssueInBrowser {
+        fun failed(exception: Exception, project: Project) {
+            NotificationGroupManager.getInstance()
+                .getNotificationGroup("TodosaurusNotifications")
+                .createNotification("Failed to open issue in browser", exception.message ?: "Unexpected error", NotificationType.ERROR)
+                .notify(project)
+        }
+    }
+}
