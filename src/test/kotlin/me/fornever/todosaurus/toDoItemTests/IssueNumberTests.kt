96281b3236efd0302319dfd9f5f1e3dfb7765ca8
Никита Великий <mtyoungstz@outlook.com>
2024-10-12 22:22:35 +0500

    Update existing tests

--- src/test/kotlin/me/fornever/todosaurus/toDoItemTests/IssueNumberTests.kt
+++ src/test/kotlin/me/fornever/todosaurus/toDoItemTests/IssueNumberTests.kt
@@ -4,7 +4,7 @@
 
 package me.fornever.todosaurus.toDoItemTests
 
-import me.fornever.todosaurus.services.ToDoItem
+import me.fornever.todosaurus.issues.ToDoItem
 import me.fornever.todosaurus.testFramework.FakeRangeMarker
 import org.junit.Assert
 import org.junit.Test
@@ -13,28 +13,28 @@ import org.junit.runners.Parameterized
 import org.junit.runners.Parameterized.Parameters
 
 @RunWith(Parameterized::class)
-class IssueNumberTests(private val source: String, private val expected: Long?) {
+class IssueNumberTests(private val source: String, private val expected: String?) {
     companion object {
         @JvmStatic
         @Parameters
         fun newItems()
             = arrayOf(
-                arrayOf("TODO[#111]:", 111L),
-                arrayOf("todo[#112]:", 112L),
-                arrayOf("text Todo[#113]:", 113L),
-                arrayOf("ToDo[#114]:", 114L),
-                arrayOf("Todo[#115]:text", 115L),
-                arrayOf("ToDo[#116]: Text", 116L),
-                arrayOf("Todo[#117]:Text", 117L),
-                arrayOf("TODO[#118]:    Text", 118L),
+                arrayOf("TODO[#111]:", "111"),
+                arrayOf("todo[#112]:", "112"),
+                arrayOf("text Todo[#113]:", "113"),
+                arrayOf("ToDo[#114]:", "114"),
+                arrayOf("Todo[#115]:text", "115"),
+                arrayOf("ToDo[#116]: Text", "116"),
+                arrayOf("Todo[#117]:Text", "117"),
+                arrayOf("TODO[#118]:    Text", "118"),
                 arrayOf("TODO", null),
                 arrayOf("todo", null),
                 arrayOf("text Todo", null),
                 arrayOf("ToDo 119", null),
-                arrayOf("Todo[120]:text", 120L),
-                arrayOf("ToDo[#121]: Text", 121L),
-                arrayOf("Todo[#122]:Text", 122L),
-                arrayOf("TODO[#123]:    Text", 123L),
+                arrayOf("Todo[120]:text", "120"),
+                arrayOf("ToDo[#121]: Text", "121"),
+                arrayOf("Todo[#122]:Text", "122"),
+                arrayOf("TODO[#123]:    Text", "123"),
                 arrayOf("TODO [124]", null),
                 arrayOf("todo [125]", null),
                 arrayOf("text Todo [#126]", null),
