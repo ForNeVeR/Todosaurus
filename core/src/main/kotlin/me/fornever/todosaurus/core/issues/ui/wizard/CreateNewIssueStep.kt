4c7bb96d1b298040e56e54737a9298a26c2fa422
Friedrich von Never <friedrich@fornever.me>
2025-01-28 23:21:04 +0100

    CreateNewIssueStep: update the copyright year back

--- core/src/main/kotlin/me/fornever/todosaurus/core/issues/ui/wizard/CreateNewIssueStep.kt
+++ core/src/main/kotlin/me/fornever/todosaurus/core/issues/ui/wizard/CreateNewIssueStep.kt
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2025 Todosaurus contributors <https://github.com/ForNeVeR/Todosaurus>
+// SPDX-FileCopyrightText: 2024-2025 Todosaurus contributors <https://github.com/ForNeVeR/Todosaurus>
 //
 // SPDX-License-Identifier: MIT
 package me.fornever.todosaurus.core.issues.ui.wizard
