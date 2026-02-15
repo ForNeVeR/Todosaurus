7da7514b301d8fee21c5c66e04e01ac9aa8db1fe
Friedrich von Never <friedrich@fornever.me>
2026-02-15 19:59:55 +0100

    Legal: fix up the license information

--- cli/Cli/Program.fs
+++ cli/Cli/Program.fs
@@ -1,2 +1,6 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
+﻿// SPDX-FileCopyrightText: 2026 Friedrich von Never <friedrich@fornever.me>
+//
+// SPDX-License-Identifier: MIT
+
+// For more information see https://aka.ms/fsharp-console-apps
 printfn "Hello from F#"
