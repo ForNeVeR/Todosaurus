41d0dc036272ecc31f927b0a524caa50a31b11e2
Nanook Claw <nanook@agentmail.to>
2026-06-04 00:13:33 +0000

    Fix: ignore git submodule entries

--- cli/Cli/GitFileProvider.fs
+++ cli/Cli/GitFileProvider.fs
@@ -9,6 +9,24 @@ open System.Collections.Generic
 open System.Threading.Tasks
 open TruePath
 
+let private gitlinkMode = "160000"
+
+let private splitNullSeparated(value: string): string array =
+    value.Split('\000', StringSplitOptions.RemoveEmptyEntries)
+
+let private tryGetPathFromCachedLsFilesEntry(entry: string): string option =
+    match entry.IndexOf('\t') with
+    | -1 -> Some entry
+    | metadataSeparator ->
+        let metadata = entry.Substring(0, metadataSeparator)
+        let relativePath = entry.Substring(metadataSeparator + 1)
+        let metadataParts = metadata.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+
+        match metadataParts with
+        | [| mode; _objectId; _stage |] when mode = gitlinkMode -> None
+        | [| _mode; _objectId; _stage |] -> Some relativePath
+        | _ -> Some entry
+
 let IsGitAvailable(): Task<bool> =
     task {
         try
@@ -21,16 +39,29 @@ let IsGitAvailable(): Task<bool> =
 
 let ListFiles(directory: AbsolutePath, includeUntracked: bool): Task<IReadOnlyList<AbsolutePath>> =
     task {
-        let args =
-            if includeUntracked then
-                [ "ls-files"; "-z"; "--cached"; "--others"; "--exclude-standard" ]
-            else
-                [ "ls-files"; "-z"; "--cached" ]
+        let! cachedResult =
+            Shell.RunProcess(directory, LocalPath "git", [ "ls-files"; "-z"; "--stage"; "--cached" ])
 
-        let! result = Shell.RunProcess(directory, LocalPath "git", args)
+        let cachedFiles =
+            cachedResult.StandardOutput
+            |> splitNullSeparated
+            |> Array.choose tryGetPathFromCachedLsFilesEntry
+
+        let! untrackedFiles =
+            if includeUntracked then
+                task {
+                    let! result =
+                        Shell.RunProcess(directory, LocalPath "git", [ "ls-files"; "-z"; "--others"; "--exclude-standard" ])
+
+                    return splitNullSeparated result.StandardOutput
+                }
+            else
+                task {
+                    return [||]
+                }
 
         return
-            result.StandardOutput.Split('\000', StringSplitOptions.RemoveEmptyEntries)
+            Array.append cachedFiles untrackedFiles
             |> Array.map(fun relativePath -> directory / relativePath)
             :> IReadOnlyList<_>
     }
